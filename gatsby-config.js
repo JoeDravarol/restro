1d3d18135a9d3a1a905dd73411d6cc1bd46f9b4b
Joe <joedravarol@gmail.com>
2020-09-24 22:39:34 +0100

    Docs: Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Restro Mediterranean  Restaurant`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Restro Mediterranean Restaurant`,
+    description: `Restro is an authentic Mediterranean Restaurant with warm friendly hospitality to create a memorable dining experience.`,
     author: `@restro`,
   },
   plugins: [
